d3edab2b5395bea7e49f67ffbd8580726616f82e
virtp <virt.p.q@yandex.ru>
2024-09-05 18:19:35 +0300

    добавил форму для емэйла и представление-класс

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        form = EmailPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post, 'form': form})
 
 
 def post_list(request):
@@ -31,3 +48,13 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
