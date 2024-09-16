86938da6bcf8ad7644507ea4db0266b37f327c39
virtp <virt.p.q@yandex.ru>
2024-09-16 18:36:31 +0300

    добавил форму с рекомендацией поста

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,36 @@
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST, request.FILES)
         if form.is_valid():
             cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you {post.title}"
+            message = f"Read {post.title} at {post_url} \n\n" \
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+
+            sent = True
     else:
         form = EmailPostForm()
     return render(request,
                   'blog/post/share.html',
-                  {'post': post, 'form': form})
+                  {'post': post, 'form': form, 'sent': sent})
 
 
 def post_list(request):
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rusakovvova54@gmail.com'
+EMAIL_HOST_PASSWORD = 'eaidkjrajzbuaplb'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
