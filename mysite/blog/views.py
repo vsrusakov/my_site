9d64cae9459596572eeed954317e15803d4c347c
virtp <virt.p.q@yandex.ru>
2024-10-27 02:11:25 +0300

    добавил вывод похожих постов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -66,11 +67,18 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
